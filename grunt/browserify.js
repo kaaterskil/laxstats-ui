8f96583a9cffa27cb185b4fd0031ea6cf6b46e0b
kaaterskil <blair@kaaterskil.com>
2015-09-19 06:28:12 -0400

    Update build

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -1,21 +1,27 @@
-/**
- * 
- */
-module.exports =
-        {
-            app: {
-                options: {
-                    transform: [['babelify', {
-                        "sourceMaps": "inline"
-                    }], 'browserify-ngannotate', 'browserify-shim', 'partialify'],
-                    external: ['angular', 'angular-animate', 'angular-messages',
-                            'angular-resource', 'angular-sanitize', 'angular-ui-router'],
-                    browserifyOptions: {
-                        debug: true,
-                        require: ['jquery', 'underscore']
-                    }
-                },
-                src: ['<%= config.source %>/app.js'],
-                dest: '<%= config.destination %>/js/<%= config.appFileName %>.js'
+'use strict';
+
+module.exports = {
+    app: {
+        options: {
+            transform: [
+                ['babelify', {"sourceMaps": "inline"}], 
+                'browserify-ngannotate', 
+                'browserify-shim', 
+                'partialify'
+            ],
+            external: [
+                'angular', 
+                'angular-animate', 
+                'angular-messages',
+                'angular-sanitize', 
+                'angular-ui-router'
+            ],
+            browserifyOptions: {
+                debug: true,
+                require: ['jquery', 'underscore']
             }
-        };
+        },
+        src: ['<%= config.source %>/app.js'],
+        dest: '<%= config.destination %>/js/<%= config.appFileName %>.js'
+    }
+};
