7e1709afa977823d67c0b8f24cabbdc47a18009e
kaaterskil <blair@kaaterskil.com>
2015-09-20 13:47:56 -0400

    Add drop-down constants

--- www/js/core/constants.js
+++ www/js/core/constants.js
@@ -1,2 +1,82 @@
 // The name of this application
 export const APP_NAME = 'laxstats';
+
+// An empty HTML template that yields to everything
+export const ABSTRACT_TEMPLATE = '<ion-nav-view></ion-nav-view>';
+
+// Regions
+export const REGIONS = [
+    {value: 'AL', label: 'Alabama'},
+    {value: 'AK', label: 'Alaska'},
+    {value: 'AZ', label: 'Arizona'},
+    {value: 'AR', label: 'Arkansas'},
+    {value: 'CA', label: 'California'},
+    {value: 'CO', label: 'Colorado'},
+    {value: 'CT', label: 'Connecticut'},
+    {value: 'DC', label: 'District of Columbia'},
+    {value: 'DE', label: 'Delaware'},
+    {value: 'FL', label: 'Florida'},
+    {value: 'GA', label: 'Georgia'},
+    {value: 'HI', label: 'Hawaii'},
+    {value: 'ID', label: 'Idaho'},
+    {value: 'IL', label: 'Illinois'},
+    {value: 'IN', label: 'Indiana'},
+    {value: 'IO', label: 'Iowa'},
+    {value: 'KS', label: 'Kansas'},
+    {value: 'KY', label: 'Kentucky'},
+    {value: 'LA', label: 'Louisiana'},
+    {value: 'ME', label: 'Maine'},
+    {value: 'MD', label: 'Maryland'},
+    {value: 'MA', label: 'Massachusetts'},
+    {value: 'MI', label: 'Michigan'},
+    {value: 'MN', label: 'Minnesota'},
+    {value: 'MS', label: 'Mississippi'},
+    {value: 'MO', label: 'Missouri'},
+    {value: 'MN', label: 'Montana'},
+    {value: 'NE', label: 'Nebraska'},
+    {value: 'NV', label: 'Nevada'},
+    {value: 'NH', label: 'New Hampshire'},
+    {value: 'NJ', label: 'New Jersey'},
+    {value: 'NM', label: 'New Mexico'},
+    {value: 'NY', label: 'New York'},
+    {value: 'NC', label: 'North Carolina'},
+    {value: 'ND', label: 'North Dakota'},
+    {value: 'OH', label: 'Ohio'},
+    {value: 'OK', label: 'Oklahoma'},
+    {value: 'OR', label: 'Oregon'},
+    {value: 'PA', label: 'Pennsylvania'},
+    {value: 'RI', label: 'Rhode Island'},
+    {value: 'SC', label: 'South Carolina'},
+    {value: 'SD', label: 'South Dakota'},
+    {value: 'TN', label: 'Tennesee'},
+    {value: 'TX', label: 'Texas'},
+    {value: 'UT', label: 'Utah'},
+    {value: 'VT', label: 'Vermont'},
+    {value: 'VA', label: 'Virgnia'},
+    {value: 'WA', label: 'Washington'},
+    {value: 'WV', label: 'West Virginia'},
+    {value: 'WI', label: 'Wisconsin'},
+    {value: 'WY', label: 'Wyoming'},
+    {value: 'AS', label: 'American Samoa'},
+    {value: 'FM', label: 'Federated States of Micronesia'},
+    {value: 'GU', label: 'Guam'},
+    {value: 'MH', label: 'Marshall Islands'},
+    {value: 'MP', label: 'Northern Mariana Islands'},
+    {value: 'PW', label: 'Palau'},
+    {value: 'PR', label: 'Puerto Rico'},
+    {value: 'VI', label: 'Virgin Islands'}
+];
+
+// Site surfaces
+export const SITE_SURFACES = [
+    {label: 'Grass', value: 'GRASS'}, 
+    {label: 'Turf', value: 'TURF'}, 
+    {label: 'Unknown', value: 'UNKNOWN'}
+];
+
+// Site styles
+export const SITE_STYLES = [
+    {label: 'Competition', value: 'COMPETITION'},
+    {label: 'Practice', value: 'PRACTICE'},
+    {label: 'Unknown', value: 'UNKNOWN'}
+];
