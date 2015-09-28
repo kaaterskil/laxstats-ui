b7dfda36ada055d8ba3fdc3dc995543d4f7920fc
kaaterskil <blair@kaaterskil.com>
2015-09-27 22:52:27 -0400

    Fix routes

---
+++ www/js/core/routes.js
@@ -0,0 +1,28 @@
+export const ROUTES = {
+    main: {
+        home: 'main/home',
+        login: 'main/login',
+        logout: 'main/logout'
+    },
+    admin: {
+        home: 'admin/home',
+        seasons: 'admin/seasons',
+        sites: 'admin/sites',
+        violations: 'admin/violations'
+    }
+};
+
+export const API = {
+    main: {
+        login: 'api/login',
+        logout:'logout',
+        schedule: 'api/schedule',
+        scoreboard: 'api/scoreboard',
+        fields: 'api/sites'
+    },
+    admin: {
+        seasons: 'api/seasons',
+        sites: 'api/sites',
+        violations: 'api/violations'
+    }
+};
